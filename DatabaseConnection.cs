5768e60ec402ab1989105ce46b2be553bec7d822
Lev Khodzhaev <khodazhaev_la@etm.ru>
2025-05-15 18:51:34 +0300

    Redesigned the code DatabaseConnection.cs

--- DatabaseConnection.cs
+++ DatabaseConnection.cs
@@ -1,26 +1,39 @@
 using System.Data.SqlClient;
+using System.IO;
 
 namespace TaskManagerDB
 {
     public static class DatabaseConnection
     {
-         public static SqlConnection Connection { get; } = CreateConnection();
+        public static SqlConnection Connection { get; } = CreateConnection();
 
-  private static SqlConnection CreateConnection()
-  {
-      string connectionString = "Data Source=LEVAXX11\\MSSQLSERVER2022;Initial Catalog=TaskManagerDB;User ID=levax; Integrated Security=True";
-      return new SqlConnection(connectionString);
-  }
+        private static SqlConnection CreateConnection()
+        {
+            string connectionString;
+            try
+            {
+                connectionString = File.ReadAllText("connection.txt").Trim();
+            }
+            catch (FileNotFoundException)
+            {
+                throw new FileNotFoundException("The connection.txt file was not found. Please create it with the SQL connection string.");
+            }
+            catch (IOException ex)
+            {
+                throw new IOException("An error occurred while reading the connection.txt file.", ex);
+            }
 
+            return new SqlConnection(connectionString);
+        }
 
-  public static void OpenConnection()
-  {
-      Connection.Open();
-  }
+        public static void OpenConnection()
+        {
+            Connection.Open();
+        }
 
-  public static void CloseConnection()
-  {
-      Connection.Close();
-  }
+        public static void CloseConnection()
+        {
+            Connection.Close();
+        }
     }
 }
